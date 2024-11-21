97b0997a559305714cde87bbc07039d5028f36c0
Lhc128 <lucashcc8@gmail.com>
2024-11-21 17:51:28 -0300

    faca a pagina inicial

--- Economation/views.py
+++ Economation/views.py
@@ -1,7 +1,5 @@
+
 from django.shortcuts import render
 
-def noticias(request):
-    return render(request, 'noticias.html')  
-
-def artigos(request):
-    return render(request, 'artigos.html')  
+def inicio(request):
+    return render(request, 'inicio.html')
